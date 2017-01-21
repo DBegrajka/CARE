08a516ae54d0a716e2a568211c324836710aa823
deepak <deepakbegrajka@gmail.com>
2017-01-21 01:25:55 -0500

    half nav

--- app/src/main/java/com/example/android/care/activity/activity/SplashActivity.java
+++ app/src/main/java/com/example/android/care/activity/activity/SplashActivity.java
@@ -1,8 +1,11 @@
-package com.example.android.care;
+package com.example.android.care.activity.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.care.activity.activity.activity.MainActivity;
+
 /**
  * Created by deepak on 1/19/17.
  */
---
+++ app/src/main/java/com/example/android/care/activity/activity/fragment/StatusFragment.java
@@ -0,0 +1,8 @@
+package com.example.android.care.activity.activity.fragment;
+
+/**
+ * Created by deepak on 1/20/17.
+ */
+
+public class StatusFragment {
+}
